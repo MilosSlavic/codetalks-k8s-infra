72eb9cc97076fd2703dde185355a2be199e43e9f
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-05-17 11:04:23 +0200

    deploy

---
+++ deploy/run.ps1
@@ -0,0 +1,4 @@
+kubectl apply -f ./deploy/helmrepository.yaml
+kubectl apply -f ./deploy/employee.helmrelease.yaml
+kubectl apply -f ./deploy/employeesearchbff.helmrelease.yaml
+kubectl apply -f ./deploy/knowledge.helmrelease.yaml
